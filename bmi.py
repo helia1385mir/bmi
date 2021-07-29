3cedd647d6e4cf315177a0e7a4bf6ecba404114d
helia1385mir <88113615+helia1385mir@users.noreply.github.com>
2021-07-29 12:44:23 +0430

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,14 @@
+
+a= float(input('vazn'))
+b= float(input('ghad'))
+bmi=a/(b*b)
+if bmi<18.5 :
+    print('underweghit')
+if 18.5<=bmi<=24.9 :
+    print('normal')
+if 25<= bmi <= 29.9 :
+    print('overweight')
+if 30<= bmi <=34.9 :
+    print("obes")
+if 35<bmi :
+    print('extermely''obes')
